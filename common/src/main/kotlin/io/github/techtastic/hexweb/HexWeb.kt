3ee35f312f9bec05cc3e74337dbe4e5efb8b9515
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-09-21 18:59:03 -0500

    The Bastard Works out of dev Version Bump

--- common/src/main/kotlin/io/github/techtastic/hexweb/HexWeb.kt
+++ common/src/main/kotlin/io/github/techtastic/hexweb/HexWeb.kt
@@ -1,25 +1,28 @@
 package io.github.techtastic.hexweb
 
 import com.mojang.datafixers.util.Either
+import dev.architectury.event.events.common.TickEvent
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import java.io.IOException
-import java.util.Optional
 import java.util.UUID
 
 object HexWeb {
     const val MOD_ID: String = "hexweb"
 
-    val client = OkHttpClient()
+    var client: OkHttpClient? = null
     val JSON = "application/json; charset=utf-8".toMediaTypeOrNull()
     val responses = mutableMapOf<UUID, Either<Response, IOException>>()
 
     @JvmStatic
     fun init() {
+        TickEvent.SERVER_PRE.register { server ->
+            this.client = OkHttpClient.Builder().build()
+        }
     }
 
     fun queueRequest(uuid: UUID, req: Request) {
-        client.newCall(req).enqueue(object: Callback {
+        client?.newCall(req)?.enqueue(object: Callback {
             override fun onFailure(call: Call, e: IOException) {
                 responses[uuid] = Either.right(e)
             }
